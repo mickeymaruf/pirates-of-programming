0bde03d51938d379598ff007dcfddf3f81480fa9
mickeymaruf <thegoldenpopo@gmail.com>
2022-10-26 18:13:22 +0600

    get single course by course id

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ app.get('/', (req, res)=>{
     res.send("Welcome to the server of Pirates of Programming.🥳")
 })
 
+app.get('/courses', (req, res)=>{
+    res.send(courses)
+})
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id
+    res.send(courses.find(course => course.id === id))
+})
+
 app.listen(port, ()=>{
     console.log(`Example app listening on port ${port}`)
 })
